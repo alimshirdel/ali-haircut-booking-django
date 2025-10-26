d5c61a5ed2e576490878c6c4e3b0497c818dea29
Ali Mohammad Shirdel <ali.m.shirdel86@gmail.com>
2025-10-26 13:59:45 +0330

    well down

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,26 +1,13 @@
-import os
 from pathlib import Path
 from decouple import config
-from dotenv import load_dotenv
-from urllib.parse import urlparse, parse_qsl
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config("DEBUG", default=True, cast=bool)  # دریافت DEBUG از .env
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["alimohammad86.pythonanywhere.com", "127.0.0.1", "localhost"]
-
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -37,7 +24,6 @@ INSTALLED_APPS = [
     "django_dramatiq",
 ]
 
-# تنظیم Redis برای Dramatiq
 DRAMATIQ_BROKER = {
     "BROKER": "dramatiq.brokers.redis.RedisBroker",
     "OPTIONS": {"url": "redis://127.0.0.1:6379/0"},
@@ -73,48 +59,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "mysite.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# --- Database Configuration (Neon + Django) ---
-
-
-load_dotenv()  # Load variables from .env
-
-tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_local_db',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": tmpPostgres.path.lstrip("/"),  # حذف / اضافی
-#         "USER": tmpPostgres.username,
-#         "PASSWORD": tmpPostgres.password,
-#         "HOST": tmpPostgres.hostname,
-#         "PORT": tmpPostgres.port or 5432,
-#         "OPTIONS": dict(parse_qsl(tmpPostgres.query)),
-#     }
-# }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -131,33 +85,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = "fa-ir"
-
 TIME_ZONE = "Asia/Tehran"
 
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "uploads"
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
@@ -165,10 +106,10 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")  # ایمیل اصلی
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  # پسورد یا App Password
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MELIPAYAMAK_USERNAME = config("MELIPAYAMAK_USERNAME")
 MELIPAYAMAK_PASSWORD = config("MELIPAYAMAK_PASSWORD")
-MELIPAYAMAK_SENDER = config("MELIPAYAMAK_SENDER")  # خط اختصاصی یا خدماتی‌ت
+MELIPAYAMAK_SENDER = config("MELIPAYAMAK_SENDER")
